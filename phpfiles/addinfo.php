1327d3b6e7df08f74863e7de1dfd3562fecb9fc2
madyan <chmasyemmadyan@gmail.com>
2022-09-14 00:21:01 +0300

    adding post feature between php and database

---
+++ phpfiles/addinfo.php
@@ -0,0 +1,17 @@
+<?php
+include("conection.php");
+
+$name= $_POST["name"];
+$email= $_POST["email"];
+$phonenb= $_POST["phonenb"];
+$messages= $_POST["messages"];
+
+$query = $mysqli->prepare("INSERT INTO `info`(name,email,phonenb,messages) VALUE (?,?,?,?)");
+$query->bind_param("ssss", $name, $email,$phonenb,$messages);
+$query->execute();
+
+$response = [];
+$response["success"] = true;
+
+echo json_encode($response);
+?>
